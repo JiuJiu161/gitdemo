f03d024fb7e3aa9a53edc7c73b378b3d038084d8
chao <542432622@qq.com>
2022-04-02 14:47:29 +0800

    push commit

--- src/main/java/com/gch/gitservice/testDemo.java
+++ src/main/java/com/gch/gitservice/testDemo.java
@@ -8,5 +8,6 @@ public class testDemo {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("push test");
     }
 }
