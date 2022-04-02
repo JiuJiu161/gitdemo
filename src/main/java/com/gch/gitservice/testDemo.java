fc99672c7bc07beecd171d19050f8b07ca656b3d
chao <542432622@qq.com>
2022-04-02 12:03:29 +0800

    first commit

---
+++ src/main/java/com/gch/gitservice/testDemo.java
@@ -0,0 +1,11 @@
+package com.gch.gitservice;
+
+/**
+ * @author Gcstart
+ * @create 2022-04-02 11:59
+ */
+public class testDemo {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
